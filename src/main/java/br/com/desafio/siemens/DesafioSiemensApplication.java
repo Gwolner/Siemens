476680caffd0de3dd241734cee93f4e2f8d6b60b
gwolner <guilherme.wdm@gmail.com>
2024-05-13 20:45:53 -0300

    feat: Projeto inicial

---
+++ src/main/java/br/com/desafio/siemens/DesafioSiemensApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.siemens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSiemensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSiemensApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/desafio/siemens/model/classes/Cliente.java
@@ -0,0 +1,13 @@
+package br.com.desafio.siemens.model.classes;
+
+public class Cliente {
+	
+	private String nomeCompleto;
+	private String sexo;
+	private String dataNascimento;
+	private int idade;
+	private String cidade;
+	
+	
+	
+}
