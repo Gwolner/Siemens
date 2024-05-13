d691f1f9fec28eae3a2826427ad997f718191483
gwolner <guilherme.wdm@gmail.com>
2024-05-13 20:50:31 -0300

    feat: "Criada as classes Cliente e Cidade"

--- src/main/java/br/com/desafio/siemens/model/classes/Cliente.java
+++ src/main/java/br/com/desafio/siemens/model/classes/Cliente.java
@@ -8,6 +8,44 @@ public class Cliente {
 	private int idade;
 	private String cidade;
 	
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
 	
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
 	
 }
