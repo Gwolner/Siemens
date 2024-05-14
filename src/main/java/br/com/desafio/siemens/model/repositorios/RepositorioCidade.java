3325160f534086e554bd1cc0c90b5bdb1699336b
gwolner <guilherme.wdm@gmail.com>
2024-05-13 23:54:15 -0300

    feat: Implementado interface em RepositorioCidade

--- src/main/java/br/com/desafio/siemens/model/repositorios/RepositorioCidade.java
+++ src/main/java/br/com/desafio/siemens/model/repositorios/RepositorioCidade.java
@@ -1,63 +1,136 @@
 package br.com.desafio.siemens.model.repositorios;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.desafio.siemens.model.classes.Cidade;
 
-public class RepositorioCidade {
+public class RepositorioCidade implements Repositorio<Cidade, String>{
 
-	private static RepositorioCidade myself = null;
-    
-    private List<Cidade> cidades = null;
-    
-    private RepositorioCidade(){
-        this.cidades = new ArrayList<>();
-    }
-    
-    public static RepositorioCidade getCurrentInstance(){
-        if(myself == null)
-            myself = new RepositorioCidade();
-        
-        return myself;
-    }
-    
-    public void inserir(Cidade cidade){
-        this.cidades.add(cidade);
-    }
-    
-    public void alterar(Cidade cidade){
-        for(Cidade c: this.cidades){
-            if(c.getNome() == cidade.getNome()){
-                c.setNome(cidade.getNome());
-                c.setEstado(cidade.getEstado());               
-            }
-        }
-    }
-    
-    public Cidade lerPorNome(String nomeCidade){
-        for(Cidade c: this.cidades){
-            if(c.getNome() == nomeCidade){
-                return c;
-            }
-        }
-        return null;
-    }
-    
-    public Cidade lerPorEstado(String estado){
-        for(Cidade c: this.cidades){
-            if(c.getEstado() == estado){
-                return c;
-            }
-        }
-        return null;
-    }
-    
-    public void excluir(Cidade cidade){
-        this.cidades.remove(cidade);
-    }
-    
-    public List<Cidade> lerTudo(){
-        return this.cidades;
-    }
+	// O modificador do construtor é default para impedir que o objeto seja criado fora do pacote
+	RepositorioCidade() {}
+	
+	@Override
+	public void inserir(Cidade cidade) throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "insert into cidade (nome, estado) values (?,?)";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		pstm.setString(1, cidade.getNome());
+		pstm.setString(2, cidade.getEstado());
+
+		pstm.execute();
+	}
+
+	@Override
+	public void alterar(Cidade cidade) throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "update cidade set tipo=?, descricao=?, preconormal=?, precocompleta=? where nome=?";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		pstm.setString(1, cidade.getNome());
+		pstm.setString(2, cidade.getEstado());
+		
+		pstm.setString(5, cidade.getNome());
+
+		pstm.execute();
+	}
+
+
+	// Ler pelo nome da cidade
+	public Cidade lerPorCidade(String nomeCidade) throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "select * from cidade where nome = ?";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		pstm.setString(1, nomeCidade);
+
+		ResultSet result = pstm.executeQuery();
+
+		Cidade tCidade = null;
+
+		if (result.next()) {
+
+			tCidade = new Cidade();
+			
+			tCidade.setNome(nomeCidade);
+			tCidade.setEstado(result.getString("estado"));			
+		}
+
+		return tCidade;
+	}
+	
+	// Ler pelo estado
+	public Cidade lerPorEstado(String nomeEstado) throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "select * from cidade where estado = ?";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		pstm.setString(1, nomeEstado);
+
+		ResultSet result = pstm.executeQuery();
+
+		Cidade tCidade = null;
+
+		if (result.next()) {
+
+			tCidade = new Cidade();
+			
+			tCidade.setNome(result.getString("nome"));
+			tCidade.setEstado(nomeEstado);						
+		}
+
+		return tCidade;
+	}
+
+	@Override
+	public void deletar(String nomeCidade) throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "delete from cidade where cidade = ?";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		pstm.setString(1, nomeCidade);
+
+		pstm.execute();
+
+	}
+
+	@Override
+	public List<Cidade> lerTudo() throws SQLException {
+		// TODO Auto-generated method stub
+
+		String sql = "select * from tipocarro";
+
+		PreparedStatement pstm = ConnectionManager.getCurrentConnection().prepareStatement(sql);
+
+		ResultSet result = pstm.executeQuery();
+
+		List<Cidade> tipos = new ArrayList<Cidade>();
+
+		while (result.next()) {
+
+			Cidade tCidade = new Cidade();
+			
+			tCidade.setNome(result.getString("nome"));
+			tCidade.setEstado(result.getString("estado"));			
+
+			tipos.add(tCidade);
+		}
+
+		return tipos;
+	}
+	
 }
